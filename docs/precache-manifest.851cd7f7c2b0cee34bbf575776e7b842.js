2539ad66707d14368173f2e1f476b15c532e568e
crod2 <carlarodriguez90@gmail.com>
2019-05-13 10:16:11 +0200

    Change build to docs

---
+++ docs/precache-manifest.851cd7f7c2b0cee34bbf575776e7b842.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579c7458f9bd4347402d48c825ebbf5f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3e28b774316e7477ad0b",
+    "url": "./static/css/main.8aba613d.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "./static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3e28b774316e7477ad0b",
+    "url": "./static/js/main.b49cb433.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
